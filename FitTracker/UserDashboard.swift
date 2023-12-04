bee0dda801c1ebdd1f07ed69183d8ee9bd1b5520
Cristopher Briseno <fr3shtricks@gmail.com>
2023-12-03 21:45:14 -0800

    User dashboard file

---
+++ FitTracker/UserDashboard.swift
@@ -0,0 +1,18 @@
+//
+//  UserDashboard.swift
+//  FitTracker
+//
+//  Created by Cris Briseno on 12/3/23.
+//
+
+import SwiftUI
+
+struct UserDashboard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserDashboard()
+}
