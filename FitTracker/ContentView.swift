ad7e1ac01c1822af801e188f373da19d24379373
Cristopher Briseno <fr3shtricks@gmail.com>
2023-12-03 21:25:53 -0800

    Initial Commit

---
+++ FitTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FitTracker
+//
+//  Created by Cris Briseno on 12/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FitTracker/FitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitTrackerApp.swift
+//  FitTracker
+//
+//  Created by Cris Briseno on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct FitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
